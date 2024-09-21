a580b2874336b111361c7bc3346ef5f90534f9f1
Danielle Starlet <dlloyd20@ivytech.edu>
2024-09-21 17:26:34 -0400

    sure

---
+++ M01_HW.py
@@ -0,0 +1,8 @@
+# Danielle Lloyd
+# Module 1 Progbramming Assignment 3.1 -3.6
+# 08/25/2024
+print("Welcome to M01 Homework Assignment")
+seconds_per_hour=60 * 60
+seconds_per_day=seconds_per_hour* 24
+test1= seconds_per_day / seconds_per_hour
+test2= seconds_per_day // seconds_per_hour
